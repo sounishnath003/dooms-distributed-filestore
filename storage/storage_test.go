401aa3e276053e5326a676975a624f6e075f7157
sounish nath <flock.sinasini@gmail.com>
2024-04-20 15:58:32 +0530

    created the CASPath for storage.engine

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"bytes"
+	"io"
 	"log"
 	"testing"
 
@@ -11,8 +12,12 @@ import (
 func TestPathTransformFunc(t *testing.T) {
 	key := "momsbestphoto"
 	log.Println("testing out the function code key")
-	pathName := CASPathTransformFunc(key)
-	assert.Equal(t, "511be51e0eb/0bacc466548/5584399005f", pathName)
+	expectedOriginalKey := "511be51e0eb0bacc4665485584399005ff19c5c3"
+	expectedPathname := "511be/51e0e/b0bac/c4665/48558/43990/05ff1/9c5c3"
+
+	pathKey := CASPathTransformFunc(key)
+	assert.Equal(t, expectedPathname, pathKey.Pathname)
+	assert.Equal(t, expectedOriginalKey, pathKey.Filename)
 }
 
 func TestStorage(t *testing.T) {
@@ -23,8 +28,15 @@ func TestStorage(t *testing.T) {
 
 	// Store initalization
 	s := NewStorage(*opts)
-	data := bytes.NewReader([]byte("some jpg bytes"))
-	if err := s.writeStream("temp", data); err != nil {
+	key := "momspecials"
+	data := []byte("some jpg bytes")
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
 		t.Error(err)
 	}
+	r, err := s.Read(key)
+	assert.Equal(t, nil, err)
+
+	b, _ := io.ReadAll(r)
+	assert.Equal(t, data, b)
+
 }
